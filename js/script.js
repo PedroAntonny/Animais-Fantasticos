01c1dce9c3de06f6155b9274bab8c4e310f6b6c6
Pedro Antonny <pedro.antonio.1597@gmail.com>
2023-10-19 12:02:08 -0300

    add modules

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from  './modules/accordion.js'
+import initiTabNav from './modules/tabnav.js'
+
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initiTabNav()
--- script.js
+++
@@ -1,9 +0,0 @@
-// const div = document.querySelector('[data-cor]')
-
-// div.dataset.heigth = 1000
-
-// console.log(div.dataset)
-
-// Adicione um atributo data-anime="show-down" e
-// data-anime="show-right" a todos as section's
-// com descricão dos animais.

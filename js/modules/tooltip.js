89a6d4f703bc60dbb475ad8de53c2384bca4c8c2
Pedro Antonny <pedro.antonio.1597@gmail.com>
2023-10-23 19:16:01 -0300

    InitAnimaNumeros

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -16,13 +16,13 @@ export default function initTooltip() {
     handleEvent() {
       this.tooltipBox.remove()
       this.element.removeEventListener("mouseleave", onMouseLeave)
-      this.element.removeEventListener('mousemove', onMouseMove)
+      this.element.removeEventListener("mousemove", onMouseMove)
     },
   }
 
   const onMouseMove = {
     handleEvent(event) {
-      this.tooltipBox.style.top = event.pageY +  20 + "px"
+      this.tooltipBox.style.top = event.pageY + 20 + "px"
       this.tooltipBox.style.left = event.pageX + 20 + "px"
     },
   }
--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ import initAccordion from  './modules/accordion.js'
 import initiTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
-import initDropdownMenu from './modules/dropdown-menu.js'
+import initDropdownMenu from "./modules/dropdown-menu.js"
 import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
 
 initScrollSuave()
 initAnimacaoScroll()
@@ -15,3 +16,4 @@ initModal()
 initTooltip()
 initDropdownMenu()
 initMenuMobile()
+initAnimaNumeros()
